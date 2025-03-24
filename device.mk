efb931d8be9d8105c16b336ad34e118c01212eee
Boris Belyaev <bbor1974@gmail.com>
2025-03-24 10:09:45 +0400

    Stop ril stuff, this is wifi only

--- device.mk
+++ device.mk
@@ -100,6 +100,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	wifi.supplicant_scan_interval=15 \
 	keyguard.no_require_sim=true
 
+ # NO ril device
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.radio.noril=true
+
 # APP CONFIGS
 PRODUCT_CHARACTERISTICS := tablet
 PRODUCT_AAPT_CONFIG := ldpi mdpi hdpi
