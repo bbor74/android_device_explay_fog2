5e2459435ff79c836f351f0b2ab5cafe8d6bbd95
Boris Belyaev <bbor1974@gmail.com>
2025-05-13 09:45:16 +0400

    BoardConfig: dont't have bluetooth

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,6 +83,10 @@ TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_LANGUAGE := ru
 TW_DEVICE_VERSION := by bbor74 
 
+# Bluetooth Configuration
+BOARD_HAVE_BLUETOOTH := false
+BOARD_HAVE_BLUETOOTH_BCM := false
+
 # TWRP_INCLUDE_LOGCAT := true
 # TARGET_USES_LOGD := true
 
