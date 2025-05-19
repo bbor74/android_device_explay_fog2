2a653fde87610918d8e3e9f9b9886e676a474fa3
Boris Belyaev <bbor1974@gmail.com>
2025-05-19 09:58:38 +0400

    Define AwesomePlayer instead of NUplayer for LP

--- device.mk
+++ device.mk
@@ -77,6 +77,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.hwui.render_dirty_regions=false
 
+# Set default player to AwesomePlayer
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.media.use-awesome=true
+
 # Debug
 ADDITIONAL_DEFAULT_PROPERTIES += \
     ro.adb.secure=0 \
