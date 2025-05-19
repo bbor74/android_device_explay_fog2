a391ea8b0c22cdbbf0198c29f807b283774decf7
Boris Belyaev <bbor1974@gmail.com>
2025-05-19 14:55:55 +0400

    Add lights.polaris

--- cm_fog2-blobs.mk
+++ cm_fog2-blobs.mk
@@ -46,3 +46,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/_prebuilt/system/etc/camera_cfg/gc0308_front/camera.cfg:system/etc/camera.cfg \
     $(LOCAL_PATH)/_prebuilt/system/etc/camera_cfg/gc0308_front/media_profiles.xml:system/etc/media_profiles.xml
 
+# Lights
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/_prebuilt/system/lib/hw/lights.polaris.so:system/lib/hw/lights.polaris.so \
+
