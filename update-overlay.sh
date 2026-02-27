5b9e3555ad0ca93baf3b6fa62405f7bcc20f03c1
Boris Belyaev <bbor1974@gmail.com>
2026-02-27 17:28:45 +0400

    First commit for lineage-14.1
    Settings: Show explay_settings page in "About phone"
    
    Overriding additional_system_update_settings is much easier than adding new entry in main menu

--- update-overlay.sh
+++ update-overlay.sh
@@ -7,22 +7,6 @@ cd "$PATCHBASE"
 
 mkdir -p overlay/packages/apps/Settings/res/xml/
 
-xmlstarlet ed \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[@android:id="@+id/about_settings"]' \
---type elem -n dashboard-tile \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
---type attr -n android:title -v "@string/device_settings" \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
---type attr -n android:icon -v "@drawable/ic_settings_profiles" \
---subnode 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
---type elem -n intent \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]/intent' \
---type attr -n android:action -v "android.intent.action.MAIN" \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]/intent' \
---type attr -n android:targetPackage -v "com.cyanogenmod.settings.device" \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]/intent' \
---type attr -n android:targetClass -v "com.cyanogenmod.settings.device.SettingsActivity" \
---insert 'dashboard-categories/dashboard-category[@android:id="@+id/system_section"]/dashboard-tile[not(@android:id)]' \
---type attr -n android:id -v "@+id/device_settings" \
-../../../packages/apps/Settings/res/xml/dashboard_categories.xml > overlay/packages/apps/Settings/res/xml/dashboard_categories.xml
-
+xmlstarlet ed --pf --ps \
+-u '/PreferenceScreen/PreferenceScreen[@android:key="additional_system_update_settings"]/@android:title' -v "@string/wifi_show_advanced" \
+../../../packages/apps/Settings/res/xml/device_info_settings.xml > overlay/packages/apps/Settings/res/xml/device_info_settings.xml
